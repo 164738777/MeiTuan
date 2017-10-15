a0d428e579eaf34be70bf7fdf9b504d17dbc0f91
LBQiang <anworkmail_q@126.com>
2017-10-15 15:31:47 +0800

    整理 附近 页面Item代码

--- src/scene/Nearby/NearbyCell.js
+++ src/scene/Nearby/NearbyCell.js
@@ -2,31 +2,30 @@
  * Copyright (c) 2017-present, Liu Jinyong
  * All rights reserved.
  *
- * https://github.com/huanxsd/MeiTuan  
+ * https://github.com/huanxsd/MeiTuan
  * @flow
  */
 
-//import liraries
-import React, { PureComponent } from 'react'
-import { View, Text, StyleSheet, TouchableOpacity, Image } from 'react-native'
-import { Heading1, Heading2, Paragraph } from '../../widget/Text'
-import { screen, system, tool } from '../../common'
-import { color } from '../../widget'
+import React, {PureComponent} from 'react'
+import {View, Text, StyleSheet, TouchableOpacity, Image} from 'react-native'
+import {Heading1, Heading2, Paragraph} from '../../widget/Text'
+import {screen, system, tool} from '../../common'
+import {color} from '../../widget'
 
-// create a component
-class NearbyCell extends PureComponent {
+// 附近 页面的List Item
+export default class NearbyCell extends PureComponent {
     render() {
-        let { info } = this.props
-        let imageUrl = info.imageUrl.replace('w.h', '160.0')
+        let {info} = this.props;
+        let imageUrl = info.imageUrl.replace('w.h', '160.0');
         return (
             <TouchableOpacity style={styles.container} onPress={() => this.props.onPress()}>
-                <Image source={{ uri: imageUrl }} style={styles.icon} />
+                <Image source={{uri: imageUrl}} style={styles.icon}/>
 
                 <View style={styles.rightContainer}>
                     <Heading1>{info.title}</Heading1>
 
-                    <Paragraph numberOfLines={0} style={{ marginTop: 8 }}>{info.subtitle}</Paragraph>
-                    <View style={{ flex: 1, justifyContent: 'flex-end' }}>
+                    <Paragraph numberOfLines={0} style={{marginTop: 8}}>{info.subtitle}</Paragraph>
+                    <View style={{flex: 1, justifyContent: 'flex-end'}}>
                         <Heading1 style={styles.price}>{info.price}元</Heading1>
                     </View>
 
@@ -36,7 +35,6 @@ class NearbyCell extends PureComponent {
     }
 }
 
-// define your styles
 const styles = StyleSheet.create({
     container: {
         flexDirection: 'row',
@@ -59,6 +57,3 @@ const styles = StyleSheet.create({
         color: color.theme
     }
 });
-
-//make this component available to the app
-export default NearbyCell;
